5ef4a82cf8b996b2ccba017f965275adb3fe36c8
Shripad <shripad1020@gmail.com>
2024-06-25 15:54:52 +0530

    header and footer

---
+++ src/app/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header>
+			<h1>Welcome to the Capstone Matching System</h1>
+			<p>Find the perfect capstone project or team for your academic journey.</p>
+		</header>
+	);
+}
