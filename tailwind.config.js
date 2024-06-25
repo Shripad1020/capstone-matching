5c9e2ba49aa44823726ef65ebd3c618bdf25326d
Shripad <shripad1020@gmail.com>
2024-06-25 15:58:21 +0530

    color for label tag

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+		colors: {
+			'label-blue': '#007bff',
+		},
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
